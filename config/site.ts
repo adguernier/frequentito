3ddf1cb1105a94d88c66710667d8349c4b04f576
erwanMarmelab <131013150+erwanMarmelab@users.noreply.github.com>
2025-10-22 10:48:02 +0200

    remove useless objects + update github link

--- config/site.ts
+++ config/site.ts
@@ -4,26 +4,7 @@ export const siteConfig = {
   name: "Frequentito",
     description:
       "An application to inform teammates our presence in the office",
-  navItems: [{}],
-  navMenuItems: [
-    {
-      label: "Frequentito",
-      href: "/",
-    },
-    {
-      label: "Profile",
-      href: "/profile",
-    },
-    {
-      label: "Login",
-      href: "/login",
-    },
-    {
-      label: "Signup",
-      href: "/signup",
-    },
-  ],
   links: {
-    github: "https://github.com/heroui-inc/heroui",
+    github: "https://github.com/adguernier/frequentito",
   },
 };
